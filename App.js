504144cbfed5df3c3947502f3742902d52aa1928
ushan1997 <ushananuka5@gmail.com>
2021-06-11 02:08:46 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/header';
+import createCategory from './components/createCatogory';
+import createVehicle from './components/createVehicle';
+import vehicles from './components/ViewVehicles';
+import catagory from './components/ViewCatogory';
+import viewVehicleInfo from './components/viewVehiclesInfo';
+import calcTripCharge from './components/calcTripCharge';
+
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <NavBar />
+                <section>
+                    <Switch>
+                        <Route path="/" component ={calcTripCharge} exact/>
+                        <Route path="/create-catogory" component={createCategory}/>
+                        <Route path="/create-vehicle" component={createVehicle}/>
+                        <Route path="/vehicles" component={vehicles}/>
+                        <Route path="/catagory" component={catagory}/>
+                        <Route path="/incatagory/:id" component={viewVehicleInfo}/>
+                    </Switch>
+                </section>
+            </Router>
+        </div>
+    );
+};
+
+export default App;
